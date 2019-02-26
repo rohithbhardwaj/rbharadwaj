6c297feb6aba8ec08ccb623bd0548cf3d21aa3c2
rohithbharadwaj <rohith.bharadwaj65@gmail.com>
2019-02-26 09:31:17 -0600

    jquery and ajax changes

--- rohith.js
+++ rohith.js
@@ -2,9 +2,27 @@ function OverMouse() {
     const xhttp = new XMLHttpRequest();
     $.ajax({
       type: 'GET',
-      url: 'https://thesimpsonsquoteapi.glitch.me/quotes?count=5',
+      url: 'https://thesimpsonsquoteapi.glitch.me/quotes?count=3',
       success: function (data) {
         $("#mouseOverme").html(data[1].quote);
       }
     });
   }
+
+  // function calConverter() 
+  //       {
+  //           var cm = document.getElementById("cm").value;
+  //           // alert("The Given" +cm+"Centimeter converted to " +inches+"inch");
+  //           var inches = (cm/2.54);
+  //           alert("The Given " +cm+" Centimeter converted to " +inches+" inch");
+  //       }
+
+  function weightConverter() {
+          var grams= document.getElementById("inputKilograms").value;
+          var kgs = grams * 1000;
+          alert("The Given  Kilograms converted to " +kgs+" grams");
+         document.getElementById("outputGrams").innerHTML = kgs;
+      }
+
+        
+        
